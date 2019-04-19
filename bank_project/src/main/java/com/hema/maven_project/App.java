43e6d8b0e7475701b98e29e927ee921c7652b406
nagaraju <nagaraju@kyatiservices>
2019-04-19 20:11:19 +0530

    bankcase

--- bank_project/src/main/java/com/hema/maven_project/App.java
+++ bank_project/src/main/java/com/hema/maven_project/App.java
@@ -1,8 +1,15 @@
 package com.hema.maven_project;
 
+import org.testng.annotations.Test;
+
 
 
 public class App {
 	
+	
+	@Test
+	public void Test_Message(){
+		System.out.println("Selenium training");
+	}
 	    
 }
